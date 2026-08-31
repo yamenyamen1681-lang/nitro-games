3e0cda855f458f3831cd7fa90d147b2e97ddcec8
yamenyamen1681-lang <yamenyamen1681@gmail.com>
2026-08-31 19:41:22 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://rucsazbldtiazjdofrem.supabase.co'
+const supabaseKey = 'sb_publishable_91VipjPX7Zf1cYGSnRSVDw_2xorDqbX'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
