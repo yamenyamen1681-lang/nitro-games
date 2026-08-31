1c3baff4621b1b274549987c888384b9e6fd44ef
yamenyamen1681-lang <yamenyamen1681@gmail.com>
2026-09-01 01:09:28 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
